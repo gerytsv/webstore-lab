f07946a5fb8ac34b45ef94f4607fdd675c0d329f
Gery <gerytsv@gmail.com>
2022-04-27 20:19:30 +0300

    init SimpleForm module

---
+++ src/app/simple-form/simple-form.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+
+@NgModule({
+  declarations: [
+    SimpleFormComponent
+  ],
+  imports: [CommonModule, BrowserModule, FormsModule],
+  exports: []
+})
+export class SimpleFormModule {}
